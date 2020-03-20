0f1c0ec1f87189f178b6312ab47080b185fa1a1f
Shintaro Tanaka <shintaro.tanaka@AQ-S539-MacBook-Pro.local>
2020-03-20 23:47:05 +0900

    add header

---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from "gatsby"
+
+class Header extends Component {
+  render() {
+    const title = this.props.title
+    return (
+      <div style={{ textAlign: `center`, backgroundColor: `steelblue` }}>
+        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `30px`}}>
+          {title}
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/layout/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+import { rhythm　} from "../../utils/typography"
+import Header from "../header"
+
+const Layout = ({ title, children }) => {
+
+  return (
+    <div>
+      <Header title={title} />
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
