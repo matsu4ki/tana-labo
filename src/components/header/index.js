8317895dd28830018d5d3e5b45521f97cac6eda8
Shintaro Tanaka <shintaro.tanaka@AQ-S539-MacBook-Pro.local>
2020-03-21 23:50:56 +0900

    update header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,9 +5,18 @@ class Header extends Component {
   render() {
     const title = this.props.title
     return (
-      <div style={{ textAlign: `center`, backgroundColor: `steelblue` }}>
-        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `30px`}}>
+      <div style={{ textAlign: `center`, backgroundColor: `steelblue`, display: `flex`, justifyContent: `flex-end` }}>
+        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `35px`, marginRight: `auto`, marginLeft: `50px`}}>
           {title}
+        </Link>
+        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `12px`, margin: `0 20px`, paddingTop: `6px` }}>
+          <div style={{ borderBottom: `solid 1px` }}>リベラルアーツ<br/><span style={{ fontSize: `8px` }}>Liberal-Arts</span></div>
+        </Link>
+        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `12px`, margin: `0 20px`, paddingTop: `6px` }}>
+          <div style={{ borderBottom: `solid 1px` }}>テクノロジー<br/><span style={{ fontSize: `8px` }}>Technology</span></div>
+        </Link>
+        <Link to={`/`} style={{ boxShadow: `none`, color: `white`, fontSize: `12px`, margin: `0 20px`, paddingTop: `6px` }}>
+          <div style={{ borderBottom: `solid 1px` }}>ビジネス<br/><span style={{ fontSize: `8px` }}>Buisiness</span></div>
         </Link>
       </div>
     )
