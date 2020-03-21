611f4d2f72178c56d8841fd2e83e17217f11652b
Shintaro Tanaka <shintaro.tanaka@AQ-S539-MacBook-Pro.local>
2020-03-21 14:01:34 +0900

    update card

--- src/components/atom/card/index.js
+++ src/components/atom/card/index.js
@@ -7,25 +7,38 @@ class Card extends Component {
     const node = this.props.node
     const title = node.frontmatter.title || node.fields.slug
     return (
-      <div>
-        <article key={node.fields.slug}>
-          <header>
-            <h3 style={{ marginBottom: rhythm(1 / 4), }}>
-              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                {title}
-              </Link>
-            </h3>
-            <small>{node.frontmatter.date}</small>
-          </header>
-          <section>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: node.frontmatter.description || node.excerpt,
-              }}
-            />
-          </section>
-        </article>
-      </div>
+      <Link to={node.fields.slug} style={{ 
+        boxShadow: `none`, 
+        color: `black`
+      }}>
+        <div style={{
+          border: `solid 1px`, 
+          borderColor: `darkgray`, 
+          boxShadow: `0 0 8px gray`, 
+          margin: `20px`, 
+          padding: `10px`,
+          width: `350px`,
+          height: `300px`
+        }}>
+          <article key={node.fields.slug}>
+            <section>
+              <p style={{ marginBottom: rhythm(1 / 4), }}>{title}</p>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }} 
+                style={{ 
+                  overflow: `hidden`,
+                  textOverflow: `ellipsis`,
+                  whiteSpace: `nowrap`,
+                  margin: `0`
+                }}
+              />
+            </section>
+          </article>
+        </div>
+      </Link>
     )
   }
 }
