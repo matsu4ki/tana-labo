ceba3a0701f0e779eec29472f6ddd6f547d3a817
Shintaro Tanaka <gogo71889@gmail.com>
2020-04-30 22:35:54 +0900

    update setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
       name: `TaNA`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `TaNA LABO is TEV Blog`,
+    siteUrl: `https://tana-labo.tokyo/`,
     social: {
       twitter: `kylemathews`,
     },
