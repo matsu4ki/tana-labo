3bc4680eb6c108cbcc5b315038302ff18958d483
Shintaro Tanaka <gogo71889@gmail.com>
2020-06-18 22:38:27 +0900

    add category

---
+++ src/components/atom/category/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Link } from "gatsby"
+
+import "./category.css"
+
+class Category extends Component {
+  render() {
+    return (
+      <div id="category">
+          <Link to={`/category/エンジニアリング/`} style={{ boxShadow: `none`, color: `black` }}>
+            <span>エンジニアリング</span>
+          </Link>
+          <Link to={`/category/テクノロジー/`} style={{ boxShadow: `none`, color: `black` }}>
+            <span>テクノロジー</span>
+          </Link>
+          <Link to={`/category/ビジネス/`} style={{ boxShadow: `none`, color: `black` }}>
+            <span>ビジネス</span>
+          </Link>
+          <Link to={`/category/リベラルアーツ/`} style={{ boxShadow: `none`, color: `black` }}>
+            <span>リベラルアーツ</span>
+          </Link>
+      </div>
+    )
+  }
+}
+
+export default Category
